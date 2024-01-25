e451650d2225dc676c793d53bd16e7b21cd863bc
rich <rajeffords@gmail.com>
2024-01-25 13:53:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Express TS Boilerplate
+
+## Introduction
+
+This is a minimal boilerplate for messing with Express and Typescript. Both frontend and backend live reload.
+
+Ngrok is throw in too in case you want to use it for some reason (e.g. testing webhooks).
+
+## Usage
+
+1. Install dependencies with `yarn` or `npm`.
+2. Secondly, rename the `example.env` file to `.env` and fill in the desired port.
+3. Run `npm start` to host the application on the specified port.
---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { NextFunction, Request, Response } from "express"
+import cors from "cors"
+import "dotenv/config"
+import fs from "fs"
+import path from "path"
+import { createServer as createLiveReloadServer } from "livereload"
+import connectLivereload from "connect-livereload"
+
+const app: express.Application = express()
+
+const liveReloadServer = createLiveReloadServer()
+liveReloadServer.watch(path.join(__dirname, "public"))
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(connectLivereload())
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).sendFile(path.join(__dirname, "public/index.html"))
+})
+
+app.get("*", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send("success")
+})
+
+app.post("*", (req: Request, res: Response, next: NextFunction) => {
+  const stringifiedBody = JSON.stringify(req.body, null, 2)
+  console.log(stringifiedBody)
+  const logString = `"${ new Date().toISOString() }": ${ stringifiedBody }\n\n--------------------------------------------------------------------------------\n\n`
+  fs.appendFile("./post-data.log", logString, (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+  res.status(200).send("[accepted]")
+})
+
+app.listen(process.env.PORT, () =>
+  console.log(`express is listening on port ${ process.env.PORT }!`),
+)
